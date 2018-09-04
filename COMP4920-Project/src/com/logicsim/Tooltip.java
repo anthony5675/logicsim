8ab350846f2a91fad4274a49cf7ffaa18259ce4b
Anthony Vu <98anthony.vu@gmail.com>
2018-09-04 15:41:05 +0000

    basic tooltip

---
+++ COMP4920-Project/src/com/logicsim/Tooltip.java
@@ -0,0 +1,36 @@
+package com.logicsim;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class Tooltip {
+    private int x, y, width, height;
+    private boolean toggled;
+
+    public Tooltip() {
+        x = 0;
+        y = 0;
+        width = 200;
+        height = 40;
+        toggled = false;
+    }
+
+    public void paint(Graphics g) {
+        g.setColor(new Color(255, 0, 255));
+        g.fillRect(x, y, width, height);
+        g.setColor(Color.WHITE);
+        g.drawString("placeholder", x + 10, y + height/2);
+    }
+
+    public int getX() { return x; }
+
+    public int getY() { return y; }
+
+    public void setX(int x) { this.x = x; }
+
+    public void setY(int y) { this.y = y; }
+
+    public boolean isToggled() { return toggled; }
+
+    public void toggleTip() { toggled = !toggled; }
+}
