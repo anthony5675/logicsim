b7433c2d11e92f0dc0e1e8a6ae7fc548994ccd82
glowku <98anthony.vu@gmail.com>
2018-10-05 00:14:22 +1000

    implement ActionListener for Toolbar

--- COMP4920-Project/src/com/logicsim/Toolbar.java
+++ COMP4920-Project/src/com/logicsim/Toolbar.java
@@ -1,25 +1,42 @@
 package com.logicsim;
 
-import java.awt.BorderLayout;
 import javax.swing.JButton;
 import javax.swing.JToolBar;
+import java.awt.BorderLayout;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
-public class Toolbar{
+public class Toolbar implements ActionListener{
 
     public Toolbar(SimWindow sw) {
         JToolBar toolbar = new JToolBar();
         toolbar.setRollover(true);
 
         JButton saveButton = new JButton("Save");
+        saveButton.setActionCommand("save");
         saveButton.setToolTipText("save the current workspace");
+        saveButton.addActionListener(this);
 
         JButton clearButton = new JButton("Clear");
+        clearButton.setActionCommand("clear");
         clearButton.setToolTipText("clear the current workspace");
-        
+        clearButton.addActionListener(this);
+
         toolbar.add(saveButton);
         toolbar.add(clearButton);
 
         sw.add(toolbar, BorderLayout.NORTH);
+    }
+
+    public void actionPerformed(ActionEvent e) {
+        String command = e.getActionCommand();
+        String output = null;
+
+        if (command.equals("save")) {
+            System.out.println("Saving workspace.");
+        } else if (command.equals("clear")) {
+            System.out.println("Clearing workspace.");
+        }
     }
 
 }
