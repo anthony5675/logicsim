d2e8cd5adfa4f763c56b046e6b509f6988e4cfb5
glowku <98anthony.vu@gmail.com>
2018-10-09 22:29:19 +1100

    tabbed workspaces with add function; remove toolbar

--- COMP4920-Project/src/com/logicsim/SimulatorWindow.java
+++ COMP4920-Project/src/com/logicsim/SimulatorWindow.java
@@ -1,6 +1,12 @@
 package com.logicsim;
 import java.awt.*;
-import javax.swing.*;
+import javax.swing.JFrame;
+import javax.swing.JTabbedPane;
+import javax.swing.JPanel;
+import javax.swing.JLabel;
+import java.util.ArrayList;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
 
 /**
  * Class to create in initialize a window for the simulator to run in
@@ -10,10 +16,19 @@ public class SimulatorWindow extends JFrame {
 
 	public static final int WINWIDTH = 800;
 	public static final int WINHEIGHT = 600;
-	
-	private SimulatorCanvas sim;
-	private SimulatorCanvas sim2;
-	
+
+	private JTabbedPane tabs;
+	// list possibly for saving feature
+	private ArrayList<SimulatorCanvas> sims = new ArrayList<SimulatorCanvas>();
+	private int numSims;
+
+	ChangeListener cl = new ChangeListener() {
+		@Override
+		public void stateChanged(ChangeEvent e) {
+			addWorkspace();
+		}
+	};
+
 	/**
 	 * Main method which runs the application overall
 	 * @param args == arguments passed in a runtime
@@ -23,62 +38,65 @@ public class SimulatorWindow extends JFrame {
 		sw.setVisible(true);
 		sw.pack();
 		sw.setResizable(false);
-
-		Thread thread = new Thread(sw.sim);
-		Thread thread2 = new Thread(sw.sim2);
-		thread.start();
-		thread2.start();
 	}
-	
+
 	/**
 	 * Initializes a Simulator Object
 	 */
 	public SimulatorWindow() {
-		
-		// Define a new main canvas and set its properties
-		sim = new SimulatorCanvas();
-		sim.setFocusable(true);
-		sim.addMouseListener(sim.getSimEngine());
-		sim.addMouseMotionListener(sim.getSimEngine());
-//		sim.addKeyListener(sim);
-		sim.setVisible(true);
-
-		sim2 = new SimulatorCanvas();
-		sim2.setFocusable(true);
-		sim2.addMouseListener(sim2.getSimEngine());
-		sim2.addMouseMotionListener(sim2.getSimEngine());
-//		sim.addKeyListener(sim);
-		sim2.setVisible(true);
-		
 		// Set basics of the window including the dimensions (using Layout) and title
 		// Put canvases onto JFrame
 		setLayout(new BorderLayout());
 		setTitle("Logic Simulator");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		initTabs();
+		add(tabs);
 		pack();
-		
+
 		// Draw the JFrame in the middle of the screen
 		setLocationRelativeTo(null);
+	}
 
-		JTabbedPane tabbedPane = new JTabbedPane();
-		JPanel one = new JPanel(new GridLayout());
-		one.add(sim);
-		tabbedPane.add("one", one);
-		JPanel two = new JPanel();
-		two.add(sim2);
-		tabbedPane.add("two", two);
+	private void initTabs() {
+		tabs = new JTabbedPane(JTabbedPane.TOP, JTabbedPane.SCROLL_TAB_LAYOUT);
+		tabs.add(createWorkspace(), numSims);
+		tabs.setTabComponentAt(numSims, new JLabel("Workspace " + String.valueOf(numSims)));
+		tabs.add(new JPanel(), "+", numSims++);
+		tabs.addChangeListener(cl);
+	}
 
-		add(tabbedPane);
+	private JPanel createWorkspace() {
+		SimulatorCanvas sim = new SimulatorCanvas();
+		sims.add(sim);
+		sim.setFocusable(true);
+		sim.addMouseListener(sim.getSimEngine());
+		sim.addMouseMotionListener(sim.getSimEngine());
+		//sim.addKeyListener(sim);
+		sim.setVisible(true);
+		JPanel panel = new JPanel(new GridLayout());
+		panel.add(sim);
+		return panel;
+	}
 
-		Toolbar tbar = new Toolbar(this, sim);
+	private void addWorkspace() {
+		int index = numSims - 1;
+		if(tabs.getSelectedIndex() == index) {
+			tabs.add(createWorkspace(), index);
+			tabs.setTabComponentAt(index, new JLabel("Workspace " + String.valueOf(index)));
+			tabs.removeChangeListener(cl);
+			tabs.setSelectedIndex(index);
+			tabs.addChangeListener(cl);
+			numSims++;
+		}
 	}
 
 	/**
-	 * Provides the simulator canvas
-	 * @return SimulatorCanvas currently displaying everything
+	 * Provides the simulator canvas list
+	 * @return SimulatorCanvas list currently displaying everything
 	 */
-	public SimulatorCanvas getSimulator() {
-		return sim;
+	public ArrayList<SimulatorCanvas> getSimulatorList() {
+		return sims;
 	}
+
 
 }
