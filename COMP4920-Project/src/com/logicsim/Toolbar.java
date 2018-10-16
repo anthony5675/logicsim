934c07b4af367713d16ce210885c36bd77ad6542
Anthony Vu <anthony.vu@pm.me>
2018-10-17 00:36:56 +1100

    simple saver without connectors

--- COMP4920-Project/src/com/logicsim/Toolbar.java
+++ COMP4920-Project/src/com/logicsim/Toolbar.java
@@ -1,11 +1,17 @@
 package com.logicsim;
 
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.io.File;
 import javax.swing.JButton;
 import javax.swing.JToolBar;
 import javax.swing.JPanel;
 import java.awt.BorderLayout;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
+import org.json.simple.*;
+import org.json.simple.parser.JSONParser;
 
 /**
  * Class to create in initialize a window for the simulator to run in
@@ -30,12 +36,18 @@ public class Toolbar extends JToolBar implements ActionListener {
         saveButton.setToolTipText("save the current workspace");
         saveButton.addActionListener(this);
 
+        JButton loadButton = new JButton("Load");
+        loadButton.setActionCommand("load");
+        loadButton.setToolTipText("load into current workspace");
+        loadButton.addActionListener(this);
+
         JButton clearButton = new JButton("Clear");
         clearButton.setActionCommand("clear");
         clearButton.setToolTipText("clear the current workspace");
         clearButton.addActionListener(this);
 
         add(saveButton);
+        add(loadButton);
         add(clearButton);
     }
 
@@ -49,9 +61,33 @@ public class Toolbar extends JToolBar implements ActionListener {
 
         if (command.equals("save")) {
             System.out.println("Saving workspace.");
+            ArrayList<Component> comps = s.getSimEngine().getComponents();
+            JSONObject root = new JSONObject();
+            JSONArray arr = new JSONArray();
+            for (Component c : comps) {
+                JSONObject comp = new JSONObject();
+                comp.put("type", c.getClass().getSimpleName());
+                comp.put("x", c.getX());
+                comp.put("y", c.getY());
+                arr.add(comp);
+            }
+            root.put("comps", arr);
+
+            File file = new File(System.getProperty("user.dir") + "/COMP4920-Project/src/com/logicsim/saves/test_save");
+
+            try (PrintWriter writer = new PrintWriter(file)) {
+                writer.print(root.toJSONString());
+
+            } catch (FileNotFoundException ex) {
+                System.out.println(ex.toString());
+            }
+
+        } else if (command.equals("load")) {
+            JSONParser parser = new JSONParser();
+
         } else if (command.equals("clear")) {
             System.out.println("Clearing workspace.");
-            s.getSimEngine().clearComponents();
+            s.getSimEngine().getComponents().clear();
         }
     }
 
