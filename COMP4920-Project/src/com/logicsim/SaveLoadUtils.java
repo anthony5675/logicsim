930be0ad2bd28468ea45ce17d765ce96a2e0ebec
Anthony Vu <anthony.vu@pm.me>
2018-10-17 11:12:38 +1100

    use another JSON parser and create save and load class

---
+++ COMP4920-Project/src/com/logicsim/SaveLoadUtils.java
@@ -0,0 +1,12 @@
+package com.logicsim;
+
+/**
+ * Class to save and load json files
+ * @author Jayden, Andre, Mitchell, Anthony
+ */
+
+public class SaveLoadUtils {
+
+
+
+}
--- COMP4920-Project/src/com/logicsim/Toolbar.java
+++ COMP4920-Project/src/com/logicsim/Toolbar.java
@@ -1,19 +1,10 @@
 package com.logicsim;
 
-import java.io.*;
-import java.util.ArrayList;
 import javax.swing.JButton;
 import javax.swing.JToolBar;
-import javax.swing.JPanel;
-import java.awt.BorderLayout;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
-import java.util.Iterator;
-import static java.lang.Math.toIntExact;
 
-import org.json.simple.*;
-import org.json.simple.parser.JSONParser;
-import org.json.simple.parser.ParseException;
 
 /**
  * Class to create in initialize a window for the simulator to run in
@@ -53,76 +44,10 @@ public class Toolbar extends JToolBar implements ActionListener {
     @Override
     public void actionPerformed(ActionEvent e) {
         String command = e.getActionCommand();
-        // TODO: Move this stuff to another class
         if (command.equals("save")) {
-            System.out.println("Saving workspace.");
-            ArrayList<Component> comps = s.getSimEngine().getComponents();
-            JSONObject root = new JSONObject();
-            JSONArray arr = new JSONArray();
-            for (Component c : comps) {
-                JSONObject comp = new JSONObject();
-                comp.put("type", c.getClass().getSimpleName());
-                comp.put("x", c.getX());
-                comp.put("y", c.getY());
-                arr.add(comp);
-            }
-            root.put("comps", arr);
-
-            File file = new File(System.getProperty("user.dir") + "/COMP4920-Project/src/com/logicsim/saves/test_save");
-
-            try (PrintWriter writer = new PrintWriter(file)) {
-                writer.print(root.toJSONString());
-
-            } catch (FileNotFoundException ex) {
-                System.out.println(ex.toString());
-            }
 
         } else if (command.equals("load")) {
-            s.getSimEngine().getComponents().clear();
-            JSONParser parser = new JSONParser();
 
-            try {
-                Object obj = parser.parse(new FileReader(System.getProperty("user.dir") + "/COMP4920-Project/src/com/logicsim/saves/test_save"));
-
-                JSONObject jsonObj = (JSONObject) obj;
-                JSONArray comps = (JSONArray) jsonObj.get("comps");
-                for(Object o: comps) {
-                    JSONObject comp = (JSONObject) o;
-                    String type = (String) comp.get("type");
-                    int x = Math.toIntExact((Long)comp.get("x"));
-                    int y = Math.toIntExact((Long)comp.get("y"));
-                    SimulatorEngine se = s.getSimEngine();
-                    Component newComp = null;
-                    switch(type) {
-                        case "Source":
-                            newComp = new Source(x, y, se);
-                            se.getComponents().add(newComp);
-                            break;
-                        case "And":
-                            newComp = new And(x, y, se);
-                            se.getComponents().add(newComp);
-                            break;
-                        case "Or":
-                            newComp = new Or(x, y, se);
-                            se.getComponents().add(newComp);
-                            break;
-                        case "Output":
-                            newComp = new Output(x, y, se);
-                            se.getComponents().add(newComp);
-                            break;
-                        default:
-                            break;
-                    }
-
-                }
-
-            } catch (FileNotFoundException ex) {
-                System.out.println(e.toString());
-            } catch (ParseException ex) {
-                System.out.println(ex.toString());
-            } catch (IOException ex) {
-                System.out.println(ex.toString());
-            }
         } else if (command.equals("clear")) {
             System.out.println("Clearing workspace.");
             s.getSimEngine().getComponents().clear();
